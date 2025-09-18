7e70d0b4ff6354843e3f31657f7a3eda08c1d042
Roel Standaert <roel.standaert@ucll.be>
2025-09-18 14:56:02 +0200

    @AutoConfigureWebTestClient annotation is not necessary

--- src/test/java/be/ucll/backend2/HttpTest.java
+++ src/test/java/be/ucll/backend2/HttpTest.java
@@ -6,13 +6,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
 @Sql("classpath:schema.sql")
 public class HttpTest {
     @Autowired
